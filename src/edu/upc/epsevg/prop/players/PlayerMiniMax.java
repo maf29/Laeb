9a837fe46fea237862c5903929017e809eb8a54f
maf29 <73944513+maf29@users.noreply.github.com>
2022-12-22 23:01:20 +0100

    working but not winnin Desdemona

--- src/edu/upc/epsevg/prop/players/PlayerMiniMax.java
+++ src/edu/upc/epsevg/prop/players/PlayerMiniMax.java
@@ -13,6 +13,7 @@ import edu.upc.epsevg.prop.othello.Move;
 import edu.upc.epsevg.prop.othello.SearchType;
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,48 +27,54 @@ public class PlayerMiniMax implements IPlayer, IAuto{
     String name = "La'eb";
     private int MAX = Integer.MAX_VALUE;
     private int MIN = Integer.MIN_VALUE;
-    
     private static final int MAX_DEPTH = 8;
-    private boolean timeout = false;
+    
+     private boolean timeout = false;
     private int TAULERS_EXAMINATS_TOTAL; //prof
     private long TAULERS_EXAMINATS;
-    //private HashMap<GameStatus, Integer> transpositionTable;
     private java.awt.Point to = new Point(2,4);
-    // Declare the hashmap or hashtable
-    Map<String, PositionScore> transpositionTable;
-
-  
+    
+    // The board is represented as a 2D array of integers.
+    // 0: empty cell
+    // 1: player 1's disc
+    // -1: player 2's disc
+    
     private int[][] board;
     private int turn;  // 1 for player 1, -1 for player 2
-
-   
+    HashMap<GameStatus, Integer> transpositionTable = new HashMap<>();
+    
     @Override
     public Move move(GameStatus gs) {
         TAULERS_EXAMINATS=0;
         TAULERS_EXAMINATS_TOTAL=1;
         ArrayList<Point> moves = gs.getMoves();
+
         if (moves.isEmpty()) {
             return null;
         }
 
         Point bestMove = moves.get(0);
-        int bestScore = Integer.MIN_VALUE;
+        int bestScore = MIN;
 
-        transpositionTable = new HashMap<>();  // for a hashmap
+        HashMap<GameStatus, Integer> transpTable = new HashMap<>();
+
         for (Point move : moves) {
             GameStatus nextBoard = new GameStatus(gs);
             nextBoard.movePiece(move);
+
             CellType opp = CellType.opposite(nextBoard.getCurrentPlayer());
             System.out.println("piece (oposite es el siguiente a jugar) = -> "+opp);
-            int score = minimax(nextBoard, MAX_DEPTH, MIN, MAX, false);
+
+            int score = minimax(nextBoard, MAX_DEPTH, MIN, MAX, false, transpTable);
+
             if (score > bestScore) {
                 bestScore = score;
                 bestMove = move;
-            }  
+            }
             TAULERS_EXAMINATS_TOTAL++;
         }
         Move move = new Move(bestMove, TAULERS_EXAMINATS, TAULERS_EXAMINATS_TOTAL, SearchType.MINIMAX);
-        
+
         return move;
     }
 
@@ -82,170 +89,138 @@ public class PlayerMiniMax implements IPlayer, IAuto{
         return name;
     }
     
-    
-    
-    class PositionScore {
-        //The PositionScore class is a simple class that stores a GameStatus object and an integer representing the best score for that position. Here is an example of how you can define the PositionScore class:
-        GameStatus gs;
-        int bestScore;
-
-        public PositionScore(GameStatus gs, int bestScore) {
-            this.gs = gs;
-            this.bestScore = bestScore;
-        }
-    }
-    
-    private int minimax(GameStatus gs, int depth, double alpha, double beta, boolean maximizingPlayer) {
-        // Check if the position is in the transposition table
-        //HashMap<GameStatus, Integer> boardHash = hashBoard(gs);
-        //transpositionTable = new HashMap<>();
-        
-         // Check if the position is in the hashmap
-        String hash = gs.toString();
-        if (transpositionTable.containsKey(hash)) {
-            return transpositionTable.get(hash).bestScore;
+    private int minimax(GameStatus gs, int depth, double alpha, double beta, boolean maximizingPlayer, HashMap<GameStatus, Integer> transpTable) {
+        // check if the current position has been evaluated before
+        Integer score = transpTable.get(gs);
+        if (score != null) {
+            // if the position has been evaluated before, return the stored score
+            return score;
         }
 
-        // Check if the search depth has been reached or the game is over
-        if (depth == 0 || gs.isGameOver()) {
-            return evaluate(gs);
+        // base case: check if the game is over or the maximum depth has been reached
+        if (gs.checkGameOver() || depth == 0) {
+            // return the score of the current position
+            return score(gs);
         }
 
-        int bestScore;
-        if (maximizingPlayer) {
-            bestScore = MIN;
-            for (Point move : gs.getMoves()) {
-                if (!gs.canMove(move, gs.getCurrentPlayer())) {
-                    continue;
-                }
-            
-                GameStatus nextBoard = new GameStatus​(gs);
-                nextBoard.movePiece​(move);
-                
-                int score = minimax(nextBoard, depth-1, alpha, beta, false);
-                bestScore = Math.max(score, bestScore);
-                
-                alpha = Math.max(alpha, bestScore);
-                if (beta <= alpha) {    //PODA alfa-beta
-                  break;
-                }
-            }
-        } else{ //
-            bestScore = MAX;
-            for (Point move : gs.getMoves()) {
-                if (!gs.canMove(move, gs.getCurrentPlayer())) {
-                    continue;
-                }
-            
-                GameStatus nextBoard = new GameStatus​(gs);
-                nextBoard.movePiece​(move);
-                
-                int score = minimax(nextBoard, depth - 1, alpha, beta, true);
-                bestScore = Math.min(score, bestScore);
-                
-                beta = Math.min(beta, bestScore);
-                if (beta <= alpha) {    //PODA alfa-beta
-                  break;
-                }
-            }
-        }
-
-        // Add the position and score to the transposition table
-        transpositionTable.put(hash, new PositionScore(gs, bestScore));
-    
-        return bestScore;
-    }
-    
-    
-    private int evaluate(GameStatus gs) {
-        // Get the scores for each player
-        int player1Score = gs.getScore(CellType.PLAYER1);
-        int player2Score = gs.getScore(CellType.PLAYER2);
-
-        // Get the list of all possible moves
+        // list of possible moves
         ArrayList<Point> moves = gs.getMoves();
+        if (maximizingPlayer) {
+            int bestScore = MIN;
+            for (Point move : moves) {
+                // simulate the move on a copy of the current board
+                GameStatus nextBoard = new GameStatus(gs);
+                nextBoard.movePiece(move);
 
-        // Filter the list of moves to get the list of moves for each player
-        List<Point> player1Moves = new ArrayList<>();
-        List<Point> player2Moves = new ArrayList<>();
-        for (Point move : moves) {
-            if (gs.getCurrentPlayer() == CellType.PLAYER1) {
-                player1Moves.add(move);
-            } else if (gs.getCurrentPlayer() == CellType.PLAYER2) {
-                player2Moves.add(move);
+                // recursive call to minimax with the updated board and reduced depth
+                int valor = minimax(nextBoard, depth - 1, alpha, beta, false, transpTable);
+
+                // update the best score for the maximizing player
+                bestScore = Math.max(bestScore, valor);
+
+                // update alpha
+                alpha = Math.max(alpha, bestScore);
+
+                // prune the tree if alpha is greater than or equal to beta
+                if (alpha >= beta) {
+                    break;
+                }
             }
+            // store the score in the transposition table
+            transpositionTable.put(gs, bestScore);
+            return bestScore;
+        } else {
+            int worstScore = MAX;
+            for (Point move : moves) {
+                // simulate the move on a copy of the current board
+                GameStatus nextBoard = new GameStatus(gs);
+                nextBoard.movePiece(move);
+
+                // recursive call to minimax with the updated board and reduced depth
+                int valor = minimax(nextBoard, depth - 1, alpha, beta, true, transpTable);
+
+                // update the worst score for the minimizing player
+                worstScore = Math.min(worstScore, valor);
+
+                // update beta
+                beta = Math.min(beta, worstScore);
+
+                // prune the tree if alpha is greater than or equal to beta
+                if (alpha >= beta) {
+                    break;
+                }
+            }
+            // store the score in the transposition table
+            transpositionTable.put(gs, worstScore);
+            return worstScore;
         }
+    }
 
-        // Calculate the mobility for each player
-        int player1Mobility = player1Moves.size();
-        int player2Mobility = player2Moves.size();
+    
+    
+    
+    
+    private int score(GameStatus gs) {
+        int score = 0;
 
-        // Calculate the stability for each player
-        int player1Stability = 0;
-        int player2Stability = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (gs.getPos(i, j) == CellType.PLAYER1) {
-                    player1Stability += calculateStability(gs, i, j, CellType.PLAYER1);
-                } else if (gs.getPos(i, j) == CellType.PLAYER2) {
-                    player2Stability += calculateStability(gs, i, j, CellType.PLAYER2);
+        // count the number of pieces on the board
+        int numPieces = 0;
+        for (int i = 0; i < gs.getSize(); i++) {
+            for (int j = 0; j < gs.getSize(); j++) {
+                if (gs.getPos(i, j) != CellType.EMPTY) {
+                    numPieces++;
                 }
             }
         }
 
-        // Calculate the potential for future flips for each player
-        int player1Potential = 0;
-        int player2Potential = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (gs.getPos(i, j) == CellType.EMPTY) {
-                    // Check the surrounding spaces
-                    if (gs.canMove(new Point(i - 1, j), CellType.PLAYER1) && gs.getPos(i - 1, j) == CellType.PLAYER1) {
-                        player1Potential++;
-                    }
-                    if (gs.canMove(new Point(i + 1, j), CellType.PLAYER1) && gs.getPos(i + 1, j) == CellType.PLAYER1) {
-                        player1Potential++;
-                    }
-                    if (gs.canMove(new Point(i, j - 1), CellType.PLAYER1) && gs.getPos(i, j - 1) == CellType.PLAYER1) {
-                        player1Potential++;
-                    }
-                    if (gs.canMove(new Point(i, j + 1), CellType.PLAYER1) && gs.getPos(i, j + 1) == CellType.PLAYER1) {
-                        player1Potential++;
-                    }
-                    if (gs.canMove(new Point(i - 1, j), CellType.PLAYER2) && gs.getPos(i - 1, j) == CellType.PLAYER2) {
-                        player2Potential++;
-                    }
-                    if (gs.canMove(new Point(i + 1, j), CellType.PLAYER2) && gs.getPos(i + 1, j) == CellType.PLAYER2) {
-                        player2Potential++;
-                    }
-                    if (gs.canMove(new Point(i, j - 1), CellType.PLAYER2) && gs.getPos(i, j - 1) == CellType.PLAYER2) {
-                        player2Potential++;
-                    }
-                    if (gs.canMove(new Point(i, j + 1), CellType.PLAYER2) && gs.getPos(i, j + 1) == CellType.PLAYER2) {
-                        player2Potential++;
-                    }
+        // assign a higher weight to the number of consecutive pieces in a row
+        int numConsecutive = 0;
+        for (int i = 0; i < gs.getSize(); i++) {
+            int consecutive = 0;
+            for (int j = 0; j < gs.getSize(); j++) {
+                if (gs.getPos(i, j) == gs.getCurrentPlayer()) {
+                    consecutive++;
+                } else {
+                    numConsecutive = Math.max(numConsecutive, consecutive);
+                    consecutive = 0;
                 }
             }
+            numConsecutive = Math.max(numConsecutive, consecutive);
         }
 
-        // Use a weighted evaluation function to combine the different factors
-        int score = (10 * (player1Score - player2Score)) +
-                    (5 * (player1Mobility - player2Mobility)) +
-                    (3 * (player1Stability - player2Stability)) +
-                    (1 * (player1Potential - player2Potential));
-
+        // add the score for the number of pieces and the number of consecutive pieces
+        score += numPieces * 10;
+        score += numConsecutive * 100;
+        
         return score;
     }
-    
-    private int calculateStability(GameStatus gs, int row, int col, CellType player) {
-        // Check if the piece is surrounded on all sides by the same player
-        if ((row > 0 && gs.getPos(row - 1, col) == player) &&
-            (row < 7 && gs.getPos(row + 1, col) == player) &&
-            (col > 0 && gs.getPos(row, col - 1) == player) &&
-            (col < 7 && gs.getPos(row, col + 1) == player)) {
-            return 1;
-        }
-        return 0;
-    }
-
+    /*public int heuristica (GameStatus t) {
+        TAULERS_EXAMINATS++;
+        int [][] values = {
+            { 120, -20,  20,   5,   5,  20, -20, 120 },
+            { -20, -40,  -5,  -5,  -5,  -5, -40, -20 },
+            {  20,  -5,  15,   3,   3,  15,  -5,  20 },
+            {   5,  -5,   3,   3,   3,   3,  -5,   5 },
+            {   5,  -5,   3,   3,   3,   3,  -5,   5 },
+            {  20,  -5,  15,   3,   3,  15,  -5,  20 },
+            { -20, -40,  -5,  -5,  -5,  -5, -40, -20 },
+            { 120, -20,  20,   5,   5,  20, -20, 120 }
+        };
+        
+        int score = 0;
+        for (int i = 0; i < t.getSize(); i++) {
+            for (int j = 0; j < t.getSize(); j++) {
+                
+                CellType piece = t.getPos​(j,i);
+                
+                if(piece == t.getCurrentPlayer()){
+                    score += values[i][j];
+                } else if(piece == CellType.opposite​(t.getCurrentPlayer())){    
+                    //conseguir jugador oponente
+                    score -= values[i][j];
+                }
+            }
+        }  
+        return score;
+    }*/
 }
